a342700cfa4d02061acd5b857237ec65ba069046
gaeunnlee <gaeunnnleee@gmail.com>
2025-12-17 17:57:26 +0900

    feat: RoomDTO createdAt 필드 추가 (#5)

--- src/main/java/room/dto/RoomDTO.java
+++ src/main/java/room/dto/RoomDTO.java
@@ -1,16 +1,22 @@
 package room.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
-@Getter @Setter
-@AllArgsConstructor @Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
 public class RoomDTO {
-    private String id;
-    private String hostUserId;
-    private String roomName;
-    private String roomPwd;
-    private int isPublic;     
-    private int playType;       
-    private int totalUserCnt;
-    private int currentUserCnt;
+	private String id;
+	private String hostUserId;
+	private String roomName;
+	private String roomPwd;
+	private int isPublic; // 0: 공개, 1: 비밀방 
+	private int playType; // 0: 개인, 1: 팀전 
+	private int totalUserCnt;
+	private int currentUserCnt;
+	private String createdAt;
 }
