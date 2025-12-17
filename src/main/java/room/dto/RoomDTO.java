82ee5591b59df65e2dffa1953f19b488f86cc370
gaeunnlee <gaeunnnleee@gmail.com>
2025-12-17 21:40:04 +0900

    feat: 방 생성 및 조회 DAO 추가 (#5)

--- src/main/java/room/dto/RoomDTO.java
+++ src/main/java/room/dto/RoomDTO.java
@@ -1,21 +1,19 @@
 package room.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @Builder
 public class RoomDTO {
 	private String id;
 	private String hostUserId;
 	private String roomName;
 	private String roomPwd;
-	private int isPublic; // 0: 공개, 1: 비밀방 
-	private int playType; // 0: 개인, 1: 팀전 
+	private String isPublic; // 0: 공개, 1: 비밀방 
+	private String playType; // 0: 개인, 1: 팀전 
 	private int totalUserCnt;
 	private int currentUserCnt;
 	private String createdAt;
