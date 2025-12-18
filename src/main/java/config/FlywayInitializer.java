62deed4b297ccdea01065df5c654dfdbe81019fc
hjh79gw <hjh79gw@gmali.com>
2025-12-19 02:02:49 +0900

    fix: db 환경변수 설정 추가 (#34)

--- src/main/java/config/FlywayInitializer.java
+++ src/main/java/config/FlywayInitializer.java
@@ -1,29 +1,41 @@
 package config;
 
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import javax.sql.DataSource;
 
 import org.flywaydb.core.Flyway;
 
 @WebListener
 public class FlywayInitializer implements ServletContextListener {
 
+	private static final String JNDI_NAME = "java:comp/env/jdbc/oracle";
+
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
-		Flyway flyway = Flyway.configure()
-			.dataSource(
-				"DB_URL",
-				"DB_NAME",
-				"DB_PWD")
-			.locations("classpath:db/migration")
-			.load();
+		try {
+			DataSource ds = lookupDataSource();
 
-		flyway.migrate();
+			Flyway flyway = Flyway.configure()
+				.dataSource(ds)
+				.locations("classpath:db/migration")
+				.load();
+
+			flyway.migrate();
+
+		} catch (Exception e) {
+			throw new RuntimeException("Flyway initialization failed (JNDI=" + JNDI_NAME + ")", e);
+		}
 	}
 
 	@Override
-	public void contextDestroyed(ServletContextEvent sce) {
-		// nothing
+	public void contextDestroyed(ServletContextEvent sce) {}
+
+	private DataSource lookupDataSource() throws NamingException {
+		InitialContext ctx = new InitialContext();
+		return (DataSource)ctx.lookup(JNDI_NAME);
 	}
 }
