3cff551ba0723e264e03fa8cda15ebefa80e039a
chanhyeok oh <125783546+ochanhyeok@users.noreply.github.com>
2025-12-18 15:51:29 +0900

    fix: GameResultDTO 주석 수정 (#13)

--- src/main/java/record/dto/GameResultDTO.java
+++ src/main/java/record/dto/GameResultDTO.java
@@ -17,8 +17,8 @@ public class GameResultDTO {
 	private String gameId; // 게임 식별
 	private String roomId; // 어떤 방에서 진행됐는지 
 	private String userId; // 플레이어 ID
-	private String stoneColor; // 흑1 백2
-	private String gameResult; // W승 L패 D무
-	private String playType; // 개인전1 팀전2
+	private String stoneColor; // 흑0 백1
+	private String gameResult; // 승0 패1 무2
+	private String playType; // 개인전0 팀전1
 	private Timestamp finishedAt; // 게임 종료 시간
 }
