04e719ba2c184ff9c2a03e71590ffacf06931b99
hjh79gw <hjh79gw@gmali.com>
2025-12-20 19:04:56 +0900

    feat: 로그아웃 + 로그인 필터 수정, 테스트페이지 삭제 (#49)

--- src/main/java/login/filter/LoginFilterChain.java
+++ src/main/java/login/filter/LoginFilterChain.java
@@ -12,7 +12,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebFilter("/lobby")
+@WebFilter(urlPatterns = {
+	"/room/*",
+	"/game/*"
+})
 public class LoginFilterChain implements Filter {
 
 	@Override
