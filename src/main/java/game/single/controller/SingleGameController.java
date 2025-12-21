b0dba74fb2a24b2bcdcf67144f4845aff2de1ac9
mac <mac@localhost>
2025-12-22 05:13:25 +0900

    feat: 개인전 채팅 웹소켓 엔드포인트 추

--- src/main/java/game/single/controller/SingleGameController.java
+++ src/main/java/game/single/controller/SingleGameController.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class SingleGameServlet
  */
-@WebServlet("/single")
+@WebServlet("/game/single")
 public class SingleGameController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
