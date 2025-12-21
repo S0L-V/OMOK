42a6577d17e310dc2ad37e6a5e9931bb85bf4299
cl-o-lc <sujin2839@gmail.com>
2025-12-21 22:16:30 +0900

    refactor: 클래스명 변경

--- src/main/java/game/multi/controller/MultiGameController.java
+++ src/main/java/game/multi/controller/MultiGameController.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/game/multi")
-public class MultiPageController extends HttpServlet {
+public class MultiGameController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 		throws ServletException, IOException {
